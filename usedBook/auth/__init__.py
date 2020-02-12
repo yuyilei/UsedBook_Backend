c72025231f1fa7349db3a9749d6dd99411ea955f
yuyilei <yyl1352091742@gmail.com>
2020-02-13 00:33:22 +0800

    feature: qiniu auth and generate token

--- usedBook/auth/__init__.py
+++ usedBook/auth/__init__.py
@@ -3,4 +3,4 @@ from flask import Blueprint
 
 auth = Blueprint('auth', __name__)
 
-from . import login
+from . import login, qiniu
---
+++ usedBook/auth/qiniu.py
@@ -0,0 +1,37 @@
+"""
+    GET /auth/qiniu/ 获取七牛的token和budget
+"""
+
+from flask import jsonify, request, g
+from . import auth
+from ..decorators import login_required
+from .. import app
+from qiniu import Auth, put_file, etag
+import qiniu.config
+import time
+
+access_key = app.config['QINIU_ACCESS_KEY']
+secret_key = app.config['QINIU_SECRET_KEY']
+bucket_name = app.config['QINIU_BUCKET_NAME']
+
+def generate_token(filename):
+    q = Auth(access_key, secret_key)
+    bucketname = "usedbook-storage"
+    token = q.upload_token(bucketname, filename, 3600)
+    return token
+
+@auth.route('/qiniu/', methods=['GET'])
+@login_required
+def qiuniu():
+    """
+    获取七牛的
+    """
+    filename = str(g.current_user.id) + str(int(time.time()))
+    return jsonify({
+            "token": generate_token(filename),
+            "bucketname": bucket_name,
+        }), 200
+
+
+
+
