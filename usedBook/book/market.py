9fa0fce5772644126df811d80c7bb70eb54399b8
yuyilei <yyl1352091742@gmail.com>
2020-02-14 17:50:18 +0800

    feature: paginate and order desc

--- usedBook/book/market.py
+++ usedBook/book/market.py
@@ -13,9 +13,14 @@ from .. import db
 @book.route('/market/', methods=['GET'])
 @login_required
 def market():
+    page = request.args.get('page', 1, type=int)
     user = User.query.filter_by(id=g.current_user.id).first()
-    books = Book.query.filter_by(is_selt=False, on_sell=True).all()
-    book_list = list(books)
+    books = Book.query.filter_by(is_selt=False, on_sell=True).order_by(Book.id.desc()).paginate(
+                page,
+                per_page=20,
+                error_out=False
+            )
+    book_list = books.items
     return jsonify({
             "message": "success",
             "book_list": len(book_list),
