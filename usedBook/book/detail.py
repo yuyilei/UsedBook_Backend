5363ebb3096affafdb48f809437e2283ddcf2546
yuyilei <yyl1352091742@gmail.com>
2020-02-17 01:49:09 +0800

    feature: return comment_list in detail api

--- usedBook/book/detail.py
+++ usedBook/book/detail.py
@@ -13,8 +13,14 @@ from .. import db
 @book.route('/detail/', methods=['GET'])
 @login_required
 def detail():
-    book_id = request.args.get('book_id')
+    page = request.args.get("page", 1, type=int)
+    per_page = 5
+    book_id = request.args.get("book_id")
     book = Book.query.filter_by(id=book_id).first()
+    comments = list(book.comments)
+    start = (page-1)*per_page
+    end = min(page*per_page, len(comments))
+    comments = comments[start:end]
     if book is None:
         return jsonify({
             "message": "book does not exist!"
@@ -22,5 +28,7 @@ def detail():
     return jsonify({
         "message": "success",
         "book": book.detail_json(g.current_user),
+        "comments": [comment.display_json() for comment in comments],
+        "comment_count": len(comments),
     }), 200
 
