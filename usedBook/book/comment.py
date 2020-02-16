29aca989ac9038957a433bc61229ea73c7b165d8
yuyilei <yyl1352091742@gmail.com>
2020-02-17 01:45:37 +0800

    feature: post comment

--- usedBook/book/comment.py
+++ usedBook/book/comment.py
@@ -1,5 +1,6 @@
 """
     POST /book/comment/ 发送评论
+    GET /book/comment/ 获取评论
 """
 
 from flask import jsonify, request, g
@@ -29,4 +30,28 @@ def post_comment():
     return jsonify({
             "message": "success",
         }), 200
+
+
+@book.route("/comment/", methods=["GET"])
+@login_required
+def get_comment():
+    book_id = request.args.get("book_id")
+    page = request.args.get("page", 1, type=int)
+    book = Book.query.filter_by(id=book_id).first()
+    if book is None:
+        return jsonify({
+                "message": "book does not exist",
+            }), 404
+
+    comments = list(book.comments)
+    per_page = 10
+    start = (page-1)* per_page
+    end = min(page*per_page, len(comments))
+    comments = comments[start:end]
+    return jsonify({
+        "message": "success",
+        "comments": [comment.to_json() for comment in comments],
+        "comment_count": len(comments),
+        }), 200
+
 
